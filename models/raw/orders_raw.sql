c18456b370b4f435db7fa5921f2069351479e040
Kalid yacqub <kalid.yacqub@thedaring.org>
2025-03-29 15:40:49 +0000

    COMMIT till customer,order and nation raw

---
+++ models/raw/orders_raw.sql
@@ -0,0 +1,6 @@
+{{ config(materialized='incremental', unique_key='O_ORDERKEY') }}
+
+SELECT src.*
+    , {{ utc_to_est_conversion() }} AS dbt_INSERT
+    , {{ utc_to_est_conversion() }} AS dbt_update
+FROM {{ source('SNOWFLAKE_SAMPLE_DATA_NAME', 'ORDERS') }} src
